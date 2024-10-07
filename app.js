e3de129099524943665c42a525e1e61fce0664ad
Stepan_Skolni <stepa.sula1@gmail.com>
2024-10-07 13:05:39 +0200

    matika skoro hotova

--- app.js
+++ app.js
@@ -9,3 +9,25 @@ if ('serviceWorker' in navigator) {
             });
     });
 }
+
+function Calculate() {
+    const a = document.getElementById("a").value;
+    const b = document.getElementById("b").value;
+    const c = document.getElementById("c").value;   
+    let res;
+    let res2;
+    let disc = (b * b) - (4 * a * c);
+    if (disc >= 0) {
+        if (disc == 0) {
+            
+        }
+        res1 = (-b + Math.sqrt(disc)) / (2*a)
+        res1 = (-b - Math.sqrt(disc)) / (2*a)
+    }
+    else
+    {
+        res = "Nelze najít výsledek v real číslech"
+    }
+
+
+}
